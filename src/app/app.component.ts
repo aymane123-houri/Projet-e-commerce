378e0ebaa972afde372a8315a363182b73b76308
aymane <houri.aymane03@gmail.com>
2024-06-08 19:30:34 +0100

    CF Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterLink,RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog-component/catalog-component.component';
+import { CartDetailComponent } from './cart-detail/cart-detail.component';
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,RouterLink,HeaderComponent,HomeComponent,CatalogComponent,CartDetailComponent,FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TP4Angular';
